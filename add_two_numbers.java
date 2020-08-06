2b12f43bdf8d6a0b6ca844ec556cd8ee38616341
Jacob22g <49438339+Jacob22g@users.noreply.github.com>
2020-08-06 18:33:28 +0300

    Add files via upload

---
+++ add_two_numbers.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+
+public class add_two_numbers {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        System.out.println(""+a+" + "+b+""+" = "+""+(a+b));
+    }
+
+}
